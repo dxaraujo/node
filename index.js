1933438c980aa3f20f7ce672a077bff1f49a8293
Daniel Xavier Araujo - DATAPREVCE <daniel.xavier@dataprev.gov.br>
2020-11-22 15:37:50 -0300

    Commit Inicial

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express')
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'whiletrue',
+    host: '139.64.244.144',
+    database: 'whiletrue',
+    password: 'whiletrue',
+    port: 5432,
+})
+const app = express()
+const port = 3000
+
+app.get('/latlon/:lat/:lon', (req, res) => {
+    pool.query("SELECT an.*, ST_Distance(ST_GeographyFromText('POINT(" + req.params.lat + " " + req.params.lon + ")'), an.location)/1000 as dist FROM anuncios an WHERE an.location is not null ORDER BY dist asc limit 200", (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+})
+
+app.get('/id/:id', (req, res) => {
+    pool.query("SELECT * FROM anuncios WHERE id_anuncios =" + req.params.id, (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Executando na porta ${port}`)
+})
