ebf9a15c9a845281fab94126d7b6780dff5d8b17
Daniel Xavier Araujo - DATAPREVCE <daniel.xavier@dataprev.gov.br>
2020-11-22 15:45:33 -0300

    Consulta de empresas

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ app.get('/id/:id', (req, res) => {
     })
 })
 
+app.get('/empresas', (req, res) => {
+    pool.query("SELECT * FROM imoveis", (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+})
+
 app.listen(port, () => {
     console.log(`Executando na porta ${port}`)
 })
