c2edbc09627dfd0c624421db8ab0b570ee5955de
Daniel Xavier Araujo - DATAPREVCE <daniel.xavier@dataprev.gov.br>
2020-11-22 17:51:39 -0300

    inserir imovel

--- index.js
+++ index.js
@@ -43,6 +43,53 @@ app.get('/imoveis', (req, res) => {
 })
 
 app.post('/imoveis', (req, res) => {
+    const query = "INSERT INTO IMOVEIS VALUE(nome, divida, city, state, street, latitude, longitude, businesstype, declared_price, price, totalareas, bedrooms, bathrooms, parkingspaces, rate, createdat, final_date, price_m2) VALUES ("
+    query += req.body.nome
+    query += ", "
+    query += req.body.divida
+    query += ", "
+    query += req.body.city
+    query += ", "
+    query += req.body.state
+    query += ", "
+    query += req.body.street
+    query += ", "
+    query += req.body.latitude
+    query += ", "
+    query += req.body.longitude
+    query += ", "
+    query += req.body.businesstype
+    query += ", "
+    query += req.body.declared_price
+    query += ", "
+    query += req.body.price
+    query += ", "
+    query += req.body.totalareas
+    query += ", "
+    query += req.body.bedrooms
+    query += ", "
+    query += req.body.bathrooms
+    query += ", "
+    query += req.body.parkingspaces
+    query += ", "
+    query += req.body.rate
+    query += ", "
+    query += req.body.createdat
+    query += ", "
+    query += req.body.final_date
+    query += ", "
+    query += req.body.price_m2
+    query += ")"
+    pool.query(query, (error, results) => {
+        
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+})
+
+app.put('/imoveis', (req, res) => {
     const id = req.body.id_imoveis;
     const price = req.body.price;
     const price_m2 = req.body.price_m2;
